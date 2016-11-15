53ee30e3da7d390b225b2bea287dce28de1b38aa
Dylan Cascio <Dcascio73@gmail.com>
2016-11-14 16:56:10 -0800

    Created db file, added students
    Added students to students.py.
    Created db file
    Looking through errors

--- students.py
+++ students.py
@@ -11,6 +11,33 @@ class Student(Model):
         database = db
 
 
+students = [{
+    'username': 'kennethlove',
+    'points': 4888
+}, {
+    'username': 'chalkers',
+    'points': 11912
+}, {
+    'username': 'joykesten2',
+    'points': 7363
+}, {
+    'username': 'craigdennis',
+    'points': 4079
+}, {
+    'username': 'davemcfarland',
+    'points': 14717
+}, {
+    'username': 'dylancascio',
+    'points': 1975
+}]
+
+
+def add_students():
+    for student in students:
+        Student.create(username=student['username'], points=student['points'])
+
+
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Student], safe=True)
+    add_students()
